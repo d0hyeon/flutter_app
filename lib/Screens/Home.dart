e0c8574c3cc22d2b05412d04999635485c691391
d0hyeon <ehgus6887@gmail.com>
2021-03-21 19:35:37 +0900

    feat: home 위젯 > Todo 위젯으로 변경

--- lib/Screens/Home.dart
+++ lib/Screens/Home.dart
@@ -1,84 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:my_app/screens/todo_form.dart';
+import 'package:my_app/widgets/todo.dart';
 
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.purple[100],
-      appBar: AppBar(title: Text('OdNh'), centerTitle: true, elevation: 0.0),
+      appBar:
+          AppBar(title: Text('Todo App'), centerTitle: true, elevation: 0.0),
       body: Padding(
-        padding: EdgeInsets.fromLTRB(30, 40, 0, 0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text('Name',
-                style: TextStyle(
-                  color: Colors.black87,
-                  letterSpacing: 2.0,
-                )
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text(
-                'BBANTO',
-                style: TextStyle(
-                  color: Colors.black87,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold)
-                ),
-              Text(
-                'BBANTO PWOER LEVEL',
-                style: TextStyle(
-                  color: Colors.black87,
-                  letterSpacing: 2.0,
-                )
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text('14',
-                style: TextStyle(
-                  color: Colors.black87,
-                  letterSpacing: 2.0,
-                  fontSize: 28,
-                  fontWeight: FontWeight.bold
-                )
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.check_circle_outline),
-                    Text(
-                      'using lightsaber', 
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        letterSpacing: 1.0
-                      )
-                    ),
-                  ]
-                )
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, 5, 0, 0),
-                child: Row(
-                  children: <Widget>[
-                    Icon(Icons.check_circle_outline),
-                    Text(
-                      'using fireball', 
-                      style: TextStyle(
-                        fontSize: 16.0,
-                        letterSpacing: 1.0
-                      )
-                    ),
-                  ]
-                )
-              )
-            ]
-          )
-        )
-      );
+          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30), child: TodoList()),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black38,
+        child: Icon(Icons.add, color: Colors.white,),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (_) => TodoForm()));
+        },
+      ),
+    );
   }
 }

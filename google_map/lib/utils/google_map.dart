a1d3911d0abd3d798c1e04996918e62caf930070
d0hyeon <ehgus6887@gmail.com>
2021-04-06 23:02:25 +0900

    feat: 구글맵 관련 유틸 파일 및 함수 추가

---
+++ google_map/lib/utils/google_map.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+Future<BitmapDescriptor> createUserMarkerIcon() {
+  return BitmapDescriptor.fromAssetImage(
+    ImageConfiguration(size: Size.square(30)), 
+      'assets/image/dot.png'
+    );
+}

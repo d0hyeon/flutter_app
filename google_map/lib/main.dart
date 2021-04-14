4408d5aa9e798ac622d107b8fb7387594bb09f2d
d0hyeon <ehgus6887@gmail.com>
2021-04-14 18:54:14 +0900

    fix: defaultBinaryMessenger 에러 수정 코드 추가

--- google_map/lib/main.dart
+++ google_map/lib/main.dart
@@ -1,7 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:my_app/screens/app.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIOverlays([]);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(App());
 }
 

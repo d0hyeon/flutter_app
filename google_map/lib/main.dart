a73f3e6e238b2f2e837998b500230d4eaca13dd3
d0hyeon <ehgus6887@gmail.com>
2021-04-15 17:59:38 +0900

    delete: 불필요 코드 제거

--- google_map/lib/main.dart
+++ google_map/lib/main.dart
@@ -1,13 +1,9 @@
-import 'dart:async';
-
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:my_app/screens/app.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await SystemChrome.setEnabledSystemUIOverlays([]);
-  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+
   runApp(App());
 }
 
